ec99b663c8349f0c32220428e95ef578d2a1f0d9
GabrielaZanetti <gabiisctt@gmail.com>
2024-07-17 11:27:40 -0300

    feat: tornar o header responsivo

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -8,14 +8,35 @@ function Header() {
     return (
         <div className="Header">
             <UIElement text='header' />
-            <div className="nav-bar">
-                <div className="icon">
-                    <img src={logo} alt="Icone Nav" />
+            <div className="header-itens">
+                <div className="nav-bar">
+                    <div className="icon">
+                        <img src={logo} alt="Icone Nav" />
+                    </div>
+                    <div className="nav">
+                        <a href="#Sobre" className="nav-item">Sobre</a>
+                        <a href="#Projetos" className="nav-item">Projetos</a>
+                        <a href="#Contatos" className="nav-item">Contatos</a>
+                    </div>
+                    <div className="nav-mobile">
+                        <div className="wrapper">
+                            <div className="checkbox-wrapper">
+                                <input type="checkbox" id="toggle" />
+                                <label className="checkbox" htmlFor="toggle">
+                                    <div className="trace"></div>
+                                    <div className="trace"></div>
+                                    <div className="trace"></div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="nav">
-                    <a href="#Sobre" className="nav-item">Sobre</a>
-                    <a href="#Projetos" className="nav-item">Projetos</a>
-                    <a href="#Contatos" className="nav-item">Contatos</a>
+                <div className="nav-mobile">
+                    <div className="itens-mobile">
+                        <a href="#Sobre" className="nav-item">Sobre</a>
+                        <a href="#Projetos" className="nav-item">Projetos</a>
+                        <a href="#Contatos" className="nav-item">Contatos</a>
+                    </div>
                 </div>
             </div>
             <UIElement text='header' fechar={true} />
