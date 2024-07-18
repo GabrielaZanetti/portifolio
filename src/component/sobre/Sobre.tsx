6e15efce764285077e88bb7fb61c0bc4d7144c91
GabrielaZanetti <gabiisctt@gmail.com>
2024-07-18 13:31:48 -0300

    fear: ajuste na descrição do sobre

--- src/component/sobre/Sobre.tsx
+++ src/component/sobre/Sobre.tsx
@@ -13,13 +13,15 @@ const Sobre: React.FC = () => {
                         Ola, sou a Gabi
                     </div>
                     <div className="mensagem">
-                        Sou Desenvolvedora Front-End, com experiência em UI/UX
+                        Sou Desenvolvedora <b>Front-End</b>, com experiência em UI/UX.
                     </div>
                     <div className="mensagem">
                         Crio Sites e Aplicativos
                     </div>
                     <div className="mensagem">
-                        Desenvolvi sites de e-commerce, um aplicativo institucional para a Unijuí, e alguns projetos de automação em Python.
+                        Desenvolvi Projetos pessoais de chat e automatização em Python.
+                        <br />
+                        Fiz manutenção do <a href="https://play.google.com/store/apps/details?id=br.edu.unijui.android&hl=pt_BR&gl=US">Aplicativo da Unijuí</a>.
                     </div>
                     <div className="mensagem">
                         Além dessas habilidades, tenho experiência em UI/UX e estou continuamente aprimorando meus conhecimentos nessa área. Também estou explorando o design e desenvolvendo projetos básicos de back-end.
