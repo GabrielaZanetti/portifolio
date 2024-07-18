f97471cbdd9564c455126a21a1e6cbf79ec5b8b7
GabrielaZanetti <gabiisctt@gmail.com>
2024-07-18 14:22:27 -0300

    feat: troca das infos dos projetos para um array

--- src/component/projects/Projetos.tsx
+++ src/component/projects/Projetos.tsx
@@ -4,13 +4,29 @@ import "./style.scss"
 import ItemLista from '../item-projeto/ItemLista';
 
 const Projetos: React.FC = () => {
+    const arrayProjetos = [
+        {
+            title: "automatização",
+            linkImg: "automatizacao.gif",
+            link: "/curso-python/tree/main/aula1",
+            repositorio: true
+        }, {
+            title: "chat",
+            linkImg: "chat.gif",
+            link: "/curso-python/tree/main/aula4",
+            repositorio: true
+        }
+    ]
+    
     return (
         <div id='Projetos'>
             <UIElement text='project' />
             <div className="container">
                 <div className="lista">
-                    <ItemLista title="automatização" linkImg="automatizacao.gif" link="/curso-python/tree/main/aula1" repositorio={true} />
-                    <ItemLista title="chat" linkImg="chat.gif" link="/curso-python/tree/main/aula4" repositorio={true} />
+                    {arrayProjetos.map(({ title, linkImg, link, repositorio }, index) => (
+                        <ItemLista key={index} title={title} linkImg={linkImg} link={link} repositorio={repositorio} />
+                    )
+                    )}
                 </div>
             </div>
             <UIElement text='project' fechar={true} />
