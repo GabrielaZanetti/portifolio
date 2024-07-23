445b8cf653dcb79931dd81c60d64a8562a420e15
GabrielaZanetti <gabiisctt@gmail.com>
2024-07-23 11:12:45 -0300

    chore: fecha o modal ao clicar fora da tela

--- src/component/modal/modal.tsx
+++ src/component/modal/modal.tsx
@@ -1,20 +1,25 @@
-// src/components/Modal.tsx
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss'
+import './style.scss';
 
 interface ModalProps {
     children: React.ReactNode;
     onClose: () => void;
-    titulo: string
+    titulo: string;
 }
 
 const Modal: React.FC<ModalProps> = ({ titulo, children, onClose }) => {
     const modalRoot = document.getElementById('modal-root') as HTMLElement;
 
-    return ReactDOM.createPortal (
-        <div className='fundo-modal'>
-            <div className="modal">
+    const handleBackgroundClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (event.target === event.currentTarget) {
+            onClose();
+        }
+    };
+
+    return ReactDOM.createPortal(
+        <div className='fundo-modal' onClick={handleBackgroundClick}>
+            <div className="modal" onClick={(e) => e.stopPropagation()}>
                 <div className="modal-titulo">
                     {titulo}
                     <button className='btn-modal' onClick={onClose}>X</button>
